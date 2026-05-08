6a6448b70ddde780e787c8673ecef546a87f2487
Sean Solis <seankun75@gmail.com>
2026-05-08 13:55:00 -0700

    Added double jump

--- Project Lumia/Assets/Code/Player/Player.cs
+++ Project Lumia/Assets/Code/Player/Player.cs
@@ -13,10 +13,14 @@ public class Player : MonoBehaviour
 
     private Animator animator;
 
+    public int extraJumpsValue = 1;
+    private int extraJumps;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+
+        extraJumps = extraJumpsValue;
     }
 
  
@@ -27,19 +31,33 @@ public class Player : MonoBehaviour
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocityY);
 
         // Makes the player jump when space is pressed
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
+            if(isGrounded)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
+            }
+            else if(extraJumps > 0)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
+                extraJumps--;
+            }
+        }
+
+        if (isGrounded)
+        {
+            extraJumps = extraJumpsValue;
         }
 
         SetAnimation(moveInput);
     }
-
+    // Checks for ground
     private void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
     }
 
+    //Plays animations depending on player movement actions
     private void SetAnimation(float moveInput)
     {
         if (isGrounded)
