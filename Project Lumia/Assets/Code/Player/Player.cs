e68dee4d5826e4dccaeb111de651254d385598f6
Sean Solis <seankun75@gmail.com>
2026-05-08 13:47:50 -0700

    fixed fmod scripts, added animations to player for running, jumping, falling, and idle

--- Project Lumia/Assets/Code/Player/Player.cs
+++ Project Lumia/Assets/Code/Player/Player.cs
@@ -10,9 +10,13 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D rb;
     private bool isGrounded;
+
+    private Animator animator;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
  
@@ -27,10 +31,38 @@ public class Player : MonoBehaviour
         {
             rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
         }
+
+        SetAnimation(moveInput);
     }
 
     private void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    private void SetAnimation(float moveInput)
+    {
+        if (isGrounded)
+        {
+            if(moveInput == 0)
+            {
+                animator.Play("Player_idle");
+            }
+            else
+            {
+                animator.Play("Player_run");
+            }
+        }
+        else
+        {
+            if(rb.linearVelocityY > 0)
+            {
+                animator.Play("Player_jump");
+            }
+            else
+            {
+                animator.Play("Player_fall");
+            }
+        }
     }
 }
